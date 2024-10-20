6916a336393f495bf87b6430195c40cba1527e90
Poienariu Vlad <vladpoienariu@gmail.com>
2024-10-20 10:18:50 +0000

    work

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+const upload = multer({dest: 'uploads/'});
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -13,6 +16,24 @@ app.get('/', function (req, res) {
 
 
 
+app.post('/api/fileanalyse', upload.any(), function(req, res) {
+  console.log(req.body);
+  console.log(req.files);
+  
+  if(req.files.length == 0) {
+    return res.json({
+      error: "No files or error"
+    })
+  }else { 
+    const file = req.files[0];
+    return res.json({
+      name: file.originalname,
+      type: file.mimetype,
+      size: file.size
+    })
+  }
+
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
